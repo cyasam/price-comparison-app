f4d1a427b295066743c87a413137e8f5449e3052
Cagdas ISIK <cyasam@gmail.com>
2019-12-16 06:34:11 +0300

    Mongo db setup

--- server/db/index.js
+++ server/db/index.js
@@ -1,10 +1,20 @@
 import mongoose from 'mongoose';
 
 export default {
-  connectDB: () =>
-    mongoose.connect('mongodb://localhost:27017/price-comparison', {
+  connectDB: async () => {
+    let mongoUserCredentials = '';
+    if (process.env.MONGO_USER && process.env.MONGO_PASSWORD) {
+      mongoUserCredentials = `${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@`;
+    }
+
+    const MONGO_URL = process.env.MONGO_URL || 'localhost:27017';
+    const DB_NAME = process.env.MONGO_DB_NAME || 'price-comparison';
+    const MONGO_CONNECTION_STRING = `mongodb://${mongoUserCredentials}${MONGO_URL}/${DB_NAME}`;
+
+    await mongoose.connect(MONGO_CONNECTION_STRING, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useFindAndModify: false
-    })
+    });
+  }
 };
